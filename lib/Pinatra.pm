145576bf21721413dc0daee9715bd961b2eab14a
Dann <techmemo@gmail.com>
2010-10-12 22:14:50 +0900

    initial commit

---
+++ lib/Pinatra.pm
@@ -0,0 +1,47 @@
+package Pinatra;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Pinatra -
+
+=head1 SYNOPSIS
+
+  use Pinatra;
+
+=head1 DESCRIPTION
+
+Pinatra is
+
+
+=head1 SOURCE AVAILABILITY
+
+This source is in Github:
+
+  http://github.com/dann/
+
+=head1 CONTRIBUTORS
+
+Many thanks to:
+
+
+=head1 AUTHOR
+
+dann E<lt>techmemo@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
